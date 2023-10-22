fe20790c7faabb49dc9d3cd68dc9f4478a91e51b
Haze272 <studiorain27@gmail.com>
2023-10-22 13:22:23 +0300

    feat: change detection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,19 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {HomePageComponent} from "../pages/home-page/home-page.component";
 import {NotFoundPageComponent} from "../pages/not-found-page/not-found-page.component";
+import {SignInPageComponent} from 'src/pages/sign-in-page/sign-in-page.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
 
-  { path: 'home', component: HomePageComponent },
+  {path: 'home', component: HomePageComponent},
 
-  { path: '**', component: NotFoundPageComponent}
+  {
+    path: 'dev',
+    children: [
+      {path: 'sign-in', title: 'Авторизация', component: SignInPageComponent}
+    ]
+  },
+
+  {path: '**', title: 'Страница не найдена!', component: NotFoundPageComponent}
 ];
--- src/pages/not-found-page/not-found-page.component.ts
+++ src/pages/not-found-page/not-found-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,7 +6,8 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './not-found-page.component.html',
-  styleUrls: ['./not-found-page.component.scss']
+  styleUrls: ['./not-found-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NotFoundPageComponent {
 
