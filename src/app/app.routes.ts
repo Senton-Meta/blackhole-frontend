72da9ca0a301f3eb18212fe2a2cedcd79ac5db14
Haze272 <studiorain27@gmail.com>
2023-10-26 23:51:07 +0300

    feat: add sign-in page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import {Routes} from '@angular/router';
 import {HomePageComponent} from "../pages/home-page/home-page.component";
 import {NotFoundPageComponent} from "../pages/not-found-page/not-found-page.component";
-import {SignInPageComponent} from 'src/pages/sign-in-page/sign-in-page.component';
+import {SignInPageComponent} from 'src/pages/auth/sign-in-page/sign-in-page.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
