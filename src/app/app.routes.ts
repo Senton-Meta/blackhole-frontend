673d49667f980fe4b2149064f0f801e08a19b6d4
Stepan Berkunov <studiorain27@gmail.com>
2023-11-03 00:14:40 +0300

    feat: add profile page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import {NotFoundPageComponent} from "../pages/not-found-page/not-found-page.comp
 import {SignInPageComponent} from 'src/pages/auth/sign-in-page/sign-in-page.component';
 import {authGuard} from "./auth/guards/auth.guard";
 import {AdminComponent} from "../pages/admin/admin.component";
+import {ProfileComponent} from "../pages/profile/profile.component";
 
 export const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
@@ -11,6 +12,7 @@ export const routes: Routes = [
   {path: 'home', component: HomePageComponent, canActivate: [authGuard]},
   {path: 'sign-in', title: 'Авторизация', component: SignInPageComponent},
 
+  { path: 'profile', component: ProfileComponent, canMatch: [authGuard]},
   { path: 'admin', component: AdminComponent, canMatch: [authGuard]},
 
   {path: '**', title: 'Страница не найдена!', component: NotFoundPageComponent}
